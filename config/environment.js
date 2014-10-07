88cde33b6b2a569880526cc0cdad9dcd211a777b
Deewendra Shrestha <dshrestha@affinnova.com>
2014-10-07 11:40:28 -0400

    updated environments for production

--- config/environment.js
+++ config/environment.js
@@ -36,6 +36,8 @@ module.exports = function(environment) {
     if (environment === 'production') {
         ENV.baseURL = 'dshrestha';
         ENV.host = 'http://www.newapassa.com/dshrestha-server/index.php';
+        //ENV.baseURL = 'dshrestha';
+        //ENV.host = 'http://localhost:8085/dshrestha-server/index.php';
     }
     return ENV;
 };
